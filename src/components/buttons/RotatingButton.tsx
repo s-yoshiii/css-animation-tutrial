35fa553152926fc396073cce82397a93d30faf33
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-08-15 01:01:48 +0900

    Rotating Button追加

---
+++ src/components/buttons/RotatingButton.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { FC } from "react";
+import styles from "./RotatingButton.module.scss";
+
+const RotatingButton: FC = () => {
+  return (
+    <Link href="/" className={styles.link}>
+      Hover me
+    </Link>
+  );
+};
+
+export default RotatingButton;
