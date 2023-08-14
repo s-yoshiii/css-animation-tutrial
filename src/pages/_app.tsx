8c12c4ff1ce1887820e13f072c4356eb15971b4d
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-08-15 00:37:51 +0900

    reset.cssを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import Home from ".";
 import "../styles/globals.scss";
+import "modern-css-reset/dist/reset.min.css";
 import { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
